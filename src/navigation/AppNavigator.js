7109be2df8f5b4e7e8999f41400cea5e0935d362
kanin2060 <maharjankanin@gmail.com>
2025-04-10 11:06:45 +0545

    Home and Joined screen done

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -3,6 +3,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignupScreen from '../screens/SignupScreen';
 import LoginScreen from '../screens/LoginScreen';
 import HomeScreen from '../screens/HomeScreen';
+import JoinedScreen from '../screens/JoinedScreen';
+
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +14,7 @@ export default function AppNavigator() {
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Signup" component={SignupScreen} />
       <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Joined" component={JoinedScreen} />
     </Stack.Navigator>
   );
 }
